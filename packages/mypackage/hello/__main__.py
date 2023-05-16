af92df9a6e22ef271fbc1bad7d10766c57c00e6c
Felix Zumstein <fzumstein@gmail.com>
2023-05-16 11:26:51 +0200

    use Book as context manager

--- packages/mypackage/hello/__main__.py
+++ packages/mypackage/hello/__main__.py
@@ -3,15 +3,14 @@ import xlwings as xw
 
 def main(args):
     # Instantiate a Book object with the deserialized request body
-    book = xw.Book(json=args)
+    with xw.Book(json=args) as book:
+        # Use xlwings as usual
+        sheet = book.sheets[0]
+        cell = sheet["A1"]
+        if cell.value == "Hello xlwings!":
+            cell.value = "Bye xlwings!"
+        else:
+            cell.value = "Hello xlwings!"
 
-    # Use xlwings as usual
-    sheet = book.sheets[0]
-    cell = sheet["A1"]
-    if cell.value == "Hello xlwings!":
-        cell.value = "Bye xlwings!"
-    else:
-        cell.value = "Hello xlwings!"
-
-    # Pass the following back as the response
-    return {"body": book.json()}
+        # Pass the following back as the response
+        return {"body": book.json()}
